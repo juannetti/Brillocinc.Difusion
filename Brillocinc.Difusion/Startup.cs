73030939130ed8be6c2aa237c0e778aa3e24e198
Nicolás Giannetti <juannetti@gmail.com>
2016-11-08 07:08:24 -0300

    Add project files.

---
+++ Brillocinc.Difusion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Brillocinc.Difusion.Startup))]
+namespace Brillocinc.Difusion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
